e69ebe090345938ac51e5aba6bcb46b3094f5f32
Felizardo <felizardopaulo40@gmail.com>
2025-02-09 11:37:44 +0200

    Melhora a responsividade das paginas principais

--- client/src/components/User/login-client.jsx
+++ client/src/components/User/login-client.jsx
@@ -80,43 +80,45 @@ const Login = () => {
   };  
 
   return (
-    <div className="auth-container">
-      <img src={logo} alt="Play 4 Me Logo" />
-      <h2>Welcome Back</h2>
-      <p>Please login with registered account</p>
-      <form onSubmit={handleSubmit}>
-        <div className="input-group">
-          <i className="bi bi-envelope"></i>
-          <input
-            type="email"
-            placeholder="Email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="input-group">
-          <i className="bi bi-lock"></i>
-          <input
-            type="password"
-            placeholder="Password"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn">Login</button>
-        {message && <p className="message">{message}</p>}
-        <p>Or using other method</p>
-        <button
-          type="button"
-          className="btn btn-alt social-btns"
-          onClick={() => loginWithGoogle()}
-        >
-          <i className="bi bi-google"></i> Sign Up with Google
-        </button>
-        <p>
-          Don&apos;t have an account? <Link to="/regist">Sign up</Link>
-        </p>
-      </form>
+    <div className="auth">
+      <div className="auth-container">
+        <img src={logo} alt="Play 4 Me Logo" />
+        <h2>Welcome Back</h2>
+        <p>Please login with registered account</p>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <i className="bi bi-envelope"></i>
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <i className="bi bi-lock"></i>
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn">Login</button>
+          {message && <p className="message">{message}</p>}
+          <p>Or using other method</p>
+          <button
+            type="button"
+            className="btn btn-alt social-btns"
+            onClick={() => loginWithGoogle()}
+          >
+            <i className="bi bi-google"></i> Sign Up with Google
+          </button>
+          <p>
+            Don&apos;t have an account? <Link to="/regist">Sign up</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
