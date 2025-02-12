6492c41c98e802d36bbe8d052b1771f09901e502
Felizardo <felizardopaulo40@gmail.com>
2025-02-12 19:54:12 +0200

    Melhora o modal e o venue profile

--- client/src/components/Venue/VenueProfile.jsx
+++ client/src/components/Venue/VenueProfile.jsx
@@ -13,26 +13,24 @@ export default function VenueProfile() {
   };
 
   return (
-    <main>
-      <img src={logo} alt="Play 4 me Logo" id={styles.logo} />
-      <header>
-        <div className={styles.searchContainer}>
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <img src={logo} alt="Play 4 me Logo" id={styles.logo} />
+        {/*<div className={styles.searchContainer}>
           <i className={`bx bx-search-alt-2 ${styles.searchIcon}`}></i>
           <input
             type="text"
             placeholder="Encontre o melhor local"
             className={styles.searchInput}
           />
-        </div>
+        </div>*/}
       </header>
 
       <section className={styles.heroSection}>
         <div className={styles.profileDescription}>
-          <div>
-            <img src={cais66Image} alt="Cais 66" id={styles.cais66Image} />
-            <div className={styles.followBtn}>
-              <button>Follow</button>
-            </div>
+          <img src={cais66Image} alt="Cais 66" id={styles.cais66Image} />
+          <div className={styles.followBtn}>
+            <button>Follow</button>
           </div>
           <div className={styles.venueProfileDescription}>
             <h1>DJ Plutônio</h1>
@@ -48,10 +46,7 @@ export default function VenueProfile() {
             </div>
             <div className={styles.paragraph}>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatibus reiciendis beatae laudantium, ab itaque quaerat
-                obcaecati tenetur earum debitis unde distinctio iusto voluptatem
-                fuga quos praesentium molestiae! Veniam, ex facere.
+                Bem vindo a conta oficial do Cais66 no Play4Me. Aqui você pode requisitar músicas e nos tocamos para você. Siga-nos para mais atualizações.
               </p>
             </div>
           </div>
@@ -59,28 +54,22 @@ export default function VenueProfile() {
       </section>
 
       <section className={styles.requestAndTipWrapper}>
-        <div>
-          <button onClick={handleRequestSong}>Request a song</button>
-        </div>
-        <div>
-          <button>Send a "Thank You" tip</button>
-        </div>
+        <button onClick={handleRequestSong}>Request a song</button>
+        <button>Send a "Thank You" tip</button>
       </section>
 
-      <section className={styles.livePlace}>
+      {/*<section className={styles.livePlace}>
         <h1 className={styles.pin}>Cais 66 está em live agora</h1>
       </section>
 
       <div className={styles.liveDescription}>
         <div className={styles.liveDescriptionContent}>
-          <div className={styles.liveDescriptionContent}>
-            <img src={cais66Image} alt="Cais 66" />
-            <h2>Descrição da live/Sessão</h2>
-            <h3>Género &gt; Afro</h3>
-            <h3>Tocando &gt; Nome da música </h3>
-          </div>
+          <img src={cais66Image} alt="Cais 66" />
+          <h2>Descrição da live/Sessão</h2>
+          <h3>Género &gt; Afro</h3>
+          <h3>Tocando &gt; Nome da música </h3>
         </div>
-      </div>
+      </div>*/}
 
       <footer>
         <p>Termos & Condições</p>
