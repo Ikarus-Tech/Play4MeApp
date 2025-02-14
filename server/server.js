e3a6367ccc242a622fa924eba7496ca967b0d2ef
Felizardo <felizardopaulo40@gmail.com>
2025-02-14 11:34:42 +0200

    Atualiza Socket listener para Requisicoes do vercel

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app); // Criando o servidor HTTP
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://play4-me-app.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
